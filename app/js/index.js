e3ffc1f0a7cf3476a709e8833beb607266ef93cc
jetreal <41300107+jetreal@users.noreply.github.com>
2019-12-27 20:46:50 +0300

    table add function now available

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,4 @@
+
 // параметры 
 var scrolled;
 window.onscroll = function () {
@@ -23,7 +24,7 @@ document.onmousemove = function (e) {
 ///////////////////////////////////////////////////////////////////////////////////
 
 
-  /*Make resizable div by Hung Nguyen*/  // and restrict to center by me //
+/*Make resizable div by Hung Nguyen*/  // and restrict to center by me //
 function makeResizableDiv(div) {
   const element = document.querySelector(div);
   const resizers = document.querySelectorAll(div + ' .resizer')
@@ -53,12 +54,13 @@ function makeResizableDiv(div) {
       if (currentResizer.classList.contains('bottom-right')) {
         getParentCenter()
         getCornerCoords()
+        getItemCoords()
 
         const width = original_width + (e.pageX - original_mouse_x);
         const height = original_height + (e.pageY - original_mouse_y)
 
         if (
-          wrapperWidth - draggedMap.offsetWidth > sumPosAndTranslLeft 
+          wrapperWidth - draggedMap.offsetWidth > sumPosAndTranslLeft
         ) {
           element.style.width = (width + 10) + 'px'
           // element.style.left = (original_x - 10) + (e.pageX - original_mouse_x) + 'px'
@@ -86,12 +88,13 @@ function makeResizableDiv(div) {
       else if (currentResizer.classList.contains('bottom-left')) {
         getParentCenter()
         getCornerCoords()
+        getItemCoords()
 
         const height = original_height + (e.pageY - original_mouse_y)
         const width = original_width - (e.pageX - original_mouse_x)
 
         if (
-          sumPosAndTranslLeft > wrapperWidth 
+          sumPosAndTranslLeft > wrapperWidth
         ) {
           element.style.width = (width + 10) + 'px'
           element.style.left = (original_x - 10) + (e.pageX - original_mouse_x) + 'px'
@@ -121,26 +124,42 @@ function makeResizableDiv(div) {
       else if (currentResizer.classList.contains('top-right')) {
         getParentCenter()
         getCornerCoords()
-  
-        const width = original_width + (e.pageX - original_mouse_x)
-        const height = original_height - (e.pageY - original_mouse_y)
+        getItemCoords()
 
-        if (
-          wrapperWidth - draggedMap.offsetWidth > sumPosAndTranslLeft 
-        ) {
+        function dropResize() {
           element.style.width = (width + 10) + 'px'
           // element.style.left = (original_x + 10) + (e.pageX - original_mouse_x) + 'px'
           var clickEvent = new Event('mouseup'); //создаем событие
           window.dispatchEvent(clickEvent); //имитируем 
           return
         }
+
+        const width = original_width + (e.pageX - original_mouse_x)
+        const height = original_height - (e.pageY - original_mouse_y)
+        console.log('width: ', width)
+        if (itemTranslateLeft + itemWidth > width - 10) {
+          dropResize()
+          return
+        }
+
+        if (
+          wrapperWidth - draggedMap.offsetWidth > sumPosAndTranslLeft
+        ) {
+          dropResize()
+          // element.style.width = (width + 10) + 'px'
+          // // element.style.left = (original_x + 10) + (e.pageX - original_mouse_x) + 'px'
+          // var clickEvent = new Event('mouseup'); //создаем событие
+          // window.dispatchEvent(clickEvent); //имитируем 
+          return
+          
+        }
         if (
           wrapperHeight < sumPosAndTranslTop
         ) {
           element.style.height = (height + 10) + 'px'
           element.style.top = (original_y - 10) + (e.pageY - original_mouse_y) + 'px'
-          var clickEvent = new Event('mouseup'); //создаем событие
-          window.dispatchEvent(clickEvent); //имитируем 
+          var clickEvent = new Event('mouseup'); // создаем событие
+          window.dispatchEvent(clickEvent); // имитируем 
           return
         }
 
@@ -153,15 +172,17 @@ function makeResizableDiv(div) {
         }
       }
       else {
-       
+
         const width = original_width - (e.pageX - original_mouse_x)
         const height = original_height - (e.pageY - original_mouse_y)
+        
 
         getParentCenter()
         getCornerCoords()
-       
+        getItemCoords()
+
         if (
-          sumPosAndTranslLeft > wrapperWidth 
+          sumPosAndTranslLeft > wrapperWidth
         ) {
           element.style.width = (width + 10) + 'px'
           element.style.left = (original_x - 10) + (e.pageX - original_mouse_x) + 'px'
@@ -187,7 +208,7 @@ function makeResizableDiv(div) {
           element.style.height = height + 'px'
           element.style.top = original_y + (e.pageY - original_mouse_y) + 'px'
         }
- 
+
       }
     }
 
@@ -198,122 +219,244 @@ function makeResizableDiv(div) {
 }
 makeResizableDiv('.resizable')
 ///////////////////////////////////////////
+const btnTable = document.querySelector( '.j-wrap-content-sidebar__add-table-btn' );
+const wrapperDraggerMap = document.querySelector('.j-wrap-content-map');
+const draggedMap = document.querySelector('.draggedMap');
+const topLeftCorner = document.querySelector('.top-left');
+const mapConner = document.querySelectorAll('.resizer');
+const addTableBtn = document.querySelector('.j-wrap-content-sidebar__add-table-btn');
+let boxItems = document.querySelectorAll( '.box-item' );
+const targetElement = document.querySelector( '.draggable-item--uno')
 
-  const wrapperDraggerMap = document.querySelector('.j-wrap-content-map');
-  const draggedMap = document.querySelector('.draggedMap');
-  const topLeftCorner = document.querySelector('.top-left');
-  const mapConner = document.querySelectorAll('.resizer');
-  const addTableBtn = document.querySelector('.j-wrap-content-sidebar__add-table-btn');
+mapConner.forEach(item => {
 
-  mapConner.forEach(item => {
-  
-    item.addEventListener('mouseover', function() {
-      Draggable.get(draggedMap).disable()
-    })
+  item.addEventListener('mouseover', function () {
+    Draggable.get(draggedMap).disable()
   })
+})
 
-  mapConner.forEach(item => {
-    item.addEventListener('mouseout', dragMap)
-  })    
-  
+mapConner.forEach(item => {
+  item.addEventListener('mouseout', dragMap)
+})
 
-  function onDrag() {
-    getParentCenter()
-    getCornerCoords()
 
-      let x, y;
-      switch (true) {
-      case (wrapperWidth  < sumPosAndTranslLeft ): {
-        Draggable.get(draggedMap).endDrag();
-        x = Draggable.get(draggedMap).x
-        y = Draggable.get(draggedMap).y
-        TweenLite.set(this.target, {x:(x - 20), y: y})
-      }
-        break;
-      case ( wrapperHeight < sumPosAndTranslTop ): {
-        Draggable.get(draggedMap).endDrag();
-        x = Draggable.get(draggedMap).x
-        y = Draggable.get(draggedMap).y
-        TweenLite.set(this.target, {x: x , y:(y - 20)})
-      }
-        break;
-        case (wrapperWidth - draggedMap.offsetWidth > sumPosAndTranslLeft  ): {
-          Draggable.get(draggedMap).endDrag();
-          x = Draggable.get(draggedMap).x
-          y = Draggable.get(draggedMap).y
-          TweenLite.set(this.target, {x: (x + 20), y: y })
-        }
-          break;
-        case ( wrapperHeight - draggedMap.offsetHeight > sumPosAndTranslTop  ): {
-          Draggable.get(draggedMap).endDrag();
-          x = Draggable.get(draggedMap).x
-          y = Draggable.get(draggedMap).y
-          TweenLite.set(this.target, {x: x , y:(y + 20)})
-        }
-          break;
-      default: return
-    }
-  }
-
-  
-  function dragMap() {
-
-    Draggable.create( draggedMap, {
-      onStart: onDrag,
-      onDrag: onDrag,
-
-    })
-  }
-  dragMap()
-    
-  // ширина и высота родителя  (вычисления центра)
-
-  let wrapperWidth;  
-  let wrapperHeight;
-  function getParentCenter() {
-    wrapperWidth = parseFloat(getComputedStyle(wrapperDraggerMap, null).getPropertyValue('width').replace('px', '')) / 2;
-    wrapperHeight = parseFloat(getComputedStyle(wrapperDraggerMap, null).getPropertyValue('height').replace('px', '')) / 2;
-    // console.log('wrapperWidth / 2: ', wrapperWidth,'wrapperHeight / 2: ', wrapperHeight)
-  }
-
-  let sumPosAndTranslLeft;
-  let sumPosAndTranslTop;
-  let posLeft;
-  let posTop;
-  function getCornerCoords() {
-    posLeft = draggedMap.offsetLeft; // координата position , без транслейт  
-    posTop = draggedMap.offsetTop;
-    const transform = draggedMap.style.transform || 'translate(0px, 0px)';
-    
-    let x = getTranslateXValue(transform)  // координата транслейта
-    let y = getTranslateYValue(transform)
-    
-      function getTranslateXValue(translateString){
-        // debugger
-        var n = translateString.indexOf("(");
-        var n1 = translateString.indexOf(",");
-        
-        var res = parseInt(translateString.slice(n+1,n1-2));
-        
-        return res;
-        
-      }
-      function getTranslateYValue(translateString){
-      
-        var n = translateString.indexOf(",");
-        var n1 = translateString.indexOf(")");
-        
-        var res = parseInt(translateString.slice(n+1,n1-1));
-      return res;
-      
-      }
-    sumPosAndTranslLeft = x + posLeft   // сумма координат
-    sumPosAndTranslTop = y + posTop
-
-  }
-
+function onDragRestrictMap() {
   getParentCenter()
   getCornerCoords()
 
+  let x, y;
+  switch (true) {
+    case (wrapperWidth < sumPosAndTranslLeft): {
+      Draggable.get(draggedMap).endDrag();
+      x = Draggable.get(draggedMap).x
+      y = Draggable.get(draggedMap).y
+      TweenLite.set(this.target, { x: (x - 20), y: y })
+    }
+      break;
+    case (wrapperHeight < sumPosAndTranslTop): {
+      Draggable.get(draggedMap).endDrag();
+      x = Draggable.get(draggedMap).x
+      y = Draggable.get(draggedMap).y
+      TweenLite.set(this.target, { x: x, y: (y - 20) })
+    }
+      break;
+    case (wrapperWidth - draggedMap.offsetWidth > sumPosAndTranslLeft): {
+      Draggable.get(draggedMap).endDrag();
+      x = Draggable.get(draggedMap).x
+      y = Draggable.get(draggedMap).y
+      TweenLite.set(this.target, { x: (x + 20), y: y })
+    }
+      break;
+    case (wrapperHeight - draggedMap.offsetHeight > sumPosAndTranslTop): {
+      Draggable.get(draggedMap).endDrag();
+      x = Draggable.get(draggedMap).x
+      y = Draggable.get(draggedMap).y
+      TweenLite.set(this.target, { x: x, y: (y + 20) })
+    }
+      break;
+    default: return
+  }
+}
 
- 
+// для перетаскивания карты
+var mapDraggble;
+function dragMap() {
+  mapDraggble = Draggable.create(draggedMap, {
+    onDrag: onDragRestrictMap,
+  })[0]
+}
+dragMap()
+
+
+// ширина и высота родителя  (вычисления центра)
+let wrapperWidth;
+let wrapperHeight;
+function getParentCenter() {
+  wrapperWidth = parseFloat(getComputedStyle(wrapperDraggerMap, null).getPropertyValue('width').replace('px', '')) / 2;
+  wrapperHeight = parseFloat(getComputedStyle(wrapperDraggerMap, null).getPropertyValue('height').replace('px', '')) / 2;
+  // console.log('wrapperWidth / 2: ', wrapperWidth,'wrapperHeight / 2: ', wrapperHeight)
+}
+
+
+let itemTranslateLeft;
+let itemTranslateTop;
+let itemWidth;
+let itemHeight;
+function getItemCoords() {
+  itemWidth = targetElement.offsetWidth
+  itemHeight = targetElement.offsetHeight
+  const itemTransform = targetElement.style.transform || 'translate(0px, 0px)';
+  itemTranslateLeft = getTranslateXValue(itemTransform);
+  itemTranslateTop = getTranslateYValue(itemTransform);
+  console.log('leftTransl: ', itemTranslateLeft, 'topTransl: ', itemTranslateTop)
+  
+}
+
+
+let sumPosAndTranslLeft;
+let sumPosAndTranslTop;
+let posLeft;
+let posTop;
+function getTranslateXValue(translateString) {
+  // debugger
+  var n = translateString.indexOf("(");
+  var n1 = translateString.indexOf(",");
+
+  var res = parseInt(translateString.slice(n + 1, n1 - 2));
+
+  return res;
+
+}
+function getTranslateYValue(translateString) {
+
+  var n = translateString.indexOf(",");
+  var n1 = translateString.indexOf(")");
+
+  var res = parseInt(translateString.slice(n + 1, n1 - 1));
+  return res;
+
+}
+
+// вычисление координат углов карты
+function getCornerCoords() {
+  posLeft = draggedMap.offsetLeft; // координата position , без транслейт  
+  posTop = draggedMap.offsetTop;
+  const transform = draggedMap.style.transform || 'translate(0px, 0px)';
+
+  let x = getTranslateXValue(transform)  // координата транслейта
+  let y = getTranslateYValue(transform)
+
+  sumPosAndTranslLeft = x + posLeft   // сумма координат транслейта и position
+  sumPosAndTranslTop = y + posTop
+}
+
+getParentCenter()
+getCornerCoords()
+
+
+//////////////////////////////////////////////////////
+
+
+// перетаскивание item'ov
+function draggable() {
+  Draggable.create( boxItems, {
+    bounds: draggedMap,
+    cursor: 'pointer',
+    type: 'x, y',
+    inertia: true,
+    onPress: onPress,
+    onRelease: onRelease,
+    onDragStart: onStart,
+    onDragEnd: function () {
+      /////////////////////////////////////////
+      function getCoords(targetElement) { // кроме IE8-
+
+        let top = targetElement.offsetTop;
+        let left = targetElement.offsetLeft;
+
+      }
+      getCoords(targetElement);
+ /////////////////////////////////////////
+
+      // ограничение карты
+      // ширина + отступ от родителя
+      // targWidth = this.x + targetElement.offsetWidth + 40;
+      // targHeight = this.y + targetElement.offsetHeight + 80;
+      // console.log(this.x)
+      // console.log(this.y)
+      // draggedMap.style.minWidth = targWidth + "px";
+      // draggedMap.style.minHeight = targHeight + "px";
+
+      boxItems.forEach(item => {
+        if (this.hitTest(item)) {
+          TweenMax.to(this.target, 1, {
+            x: xx,
+            y: yy,
+            // rotate: 90
+          });
+        }
+      })
+    }
+  })
+}
+draggable()
+
+
+var xx = 0;
+var yy = 0;
+function onPress() {
+  xx = this.x;
+  yy = this.y;
+
+const tlPress = new TimelineMax();
+tlPress.to( this.target, 0.1, {
+  borderColor: 'white',
+  borderWidth: 6,
+  ease: Power4.easeIn
+})
+}
+// -------------------------------------
+function onStart() {
+  mapDraggble.disable();
+
+  const tlPress = new TimelineMax();
+  tlPress.to( this.target, 0.1, {
+    opacity: 1,
+    borderColor: 'red',
+    borderWidth: 2,
+    ease: Power4.easeIn
+  })
+}
+// ---------------------------------
+function onRelease() {
+  mapDraggble.enable();
+
+  const tlPress = new TimelineMax();
+  tlPress.to(this.target, 0.1, {
+    // borderColor: 'green',
+    borderWidth: 3,
+    ease: Power4.easeIn,
+  })
+}
+
+
+/////////////////////////////////////////////////////////////
+  // создание стола
+btnTable.addEventListener('click', function() {
+  let div = document.createElement('div');
+  div.classList.add('box-item');
+  div.classList.add('draggable-item');
+  let span = document.createElement('span');
+  span.innerHTML = 'X';
+  span.classList.add('spanClose');
+  span.addEventListener('click', function() {
+    div.parentNode.removeChild(div)
+  })
+  draggedMap.appendChild(div)
+  div.appendChild(span)
+  boxItems = document.querySelectorAll( '.box-item' );
+  draggable()
+})
+
+
+
